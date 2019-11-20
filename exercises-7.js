31e07ad492dcfb62cb305f692bd3402d5e35a218
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-20 11:44:05 +0700

    Pembaharuan exercise 7

--- exercises-7.js
+++ exercises-7.js
@@ -1,35 +1,34 @@
 //Tanyain Ke Instruktur apakah harus pake console ???
 
 //Soal Nomor 1
-document.write('Soal Nomor 1')
-document.write('<br>')
+console.log('Soal Nomor 1')
 var loopingTimes = 5;
 
-for(i = 0; i < loopingTimes; i++){
-    document.write('*');
-    document.write('<br>')
-}
-
-document.write('<br>')
-
-document.write('Soal Nomor 2')
-document.write('<br>')
-
-for(i = 0; i < loopingTimes; i++){
-    for(j = 0; j < loopingTimes; j++){
-        document.write('*')
-    }
-    document.write('<br>')
-}
-
-document.write('<br>')
-
-document.write('Soal Nomor 3')
-document.write('<br>')
-
 for(i = 0; i < loopingTimes;i++){
-    for(j = 0; j <= i; j++){
-        document.write('*')
-    }
-    document.write('<br>')
+    var starContainer = ''
+    starContainer += '*'
+
+    console.log(starContainer)
 }
+
+
+console.log('Soal Nomor 2')
+for(i = 0; i < loopingTimes;i++){
+    var starContainer = ''
+    for(j = 0; j < loopingTimes;j++){
+        starContainer += '*'
+    }
+
+    console.log(starContainer)
+}
+
+
+console.log('Soal Nomor 3')
+for(i = 0; i < loopingTimes;i++){
+    var starContainer = ''
+    for(j = 0; j <= i;j++){
+        starContainer += '*'
+    }
+    console.log(starContainer)
+}
+
